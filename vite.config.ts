0d31402473f615c24610ec0ce0c59558ac71c762
nettemsarath <nettemsarath1@gmail.com>
2024-10-26 16:11:47 +0530

    fix: fix path name in vite config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "./protfolio/",
+  base: "/protfolio/",
   plugins: [react()],
 });
